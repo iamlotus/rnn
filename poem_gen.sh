e7b6771c8381886dc2de0d135896ae79b54cae4c
lotus.jzx <lotus.jzx@alibaba-inc.com>
2018-08-29 19:12:43 +0800

    modified

--- poem_gen.sh
+++ poem_gen.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-python3 poem.py --mode=gen --cuda_visible_devices=1
+python3 poem.py --mode=gen --cuda_visible_devices=1 --file_path=./data/poems.txt --epochs=10000 --training_echo_interval=200 --training_save_interval=2000
 
 
