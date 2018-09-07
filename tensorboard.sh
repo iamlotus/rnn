a3be71d647e804d71504fb52044f925f536a7922
lotus.jzx <lotus.jzx@alibaba-inc.com>
2018-09-07 10:53:46 +0800

    modified

--- tensorboard.sh
+++ tensorboard.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
+pid=`ps -ef |grep tensorboard |grep -v grep | grep -v tensorboard.sh | awk '{print $2}'`
+if [ -n "$pid" ]; then
+        echo shutdown current tensorboard pid $pid
+        kill $pid
+fi
+
 nohup tensorboard --port 10086 --logdir=logs/ > logs/tensorboard.out 2>&1 &
-
-
-
+echo start tensorboard
