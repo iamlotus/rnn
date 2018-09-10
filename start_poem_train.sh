e70871e1560cc55b44505630c961e3966f0539be
lotus.jzx <lotus.jzx@alibaba-inc.com>
2018-09-10 19:18:32 +0800

    modified

--- start_poem_train.sh
+++ start_poem_train.sh
@@ -9,12 +9,12 @@ if [ -f ".trainpid" ]; then
     else
         rm .trainpid \
         && echo [remove dead pid `cat .trainpid`] \
-        && nohup python3 poem.py --mode=train --cell_type=lstm --learning_rate=0.00001 --batch_size=16 --train_file_path=./data/poems.txt --validate_file_path=none --epochs=10000 --training_echo_interval=500 --validate_echo_interval=200 --training_save_interval=5000 >logs/train.out 2>&1 & echo $! > .trainpid \
+        && nohup python3 poem.py --mode=train --cell_type=rnn --rnn_size=256 --learning_rate=0.00001 --batch_size=16 --train_file_path=./data/poems.txt --validate_file_path=none --epochs=10000 --training_echo_interval=500 --validate_echo_interval=200 --training_save_interval=5000 >logs/train.out 2>&1 & echo $! > .trainpid \
         && echo [train started] \
         && busybox tail -f logs/train.out
     fi
 else
-    nohup python3 poem.py --mode=train --cell_type=lstm --learning_rate=0.00001 --batch_size=16 --train_file_path=./data/poems.txt --validate_file_path=none --epochs=10000 --training_echo_interval=500 --validate_echo_interval=200 --training_save_interval=5000 >logs/train.out 2>&1 & echo $! > .trainpid \
+    nohup python3 poem.py --mode=train --cell_type=rnn --rnn_size=256 --learning_rate=0.00001 --batch_size=16 --train_file_path=./data/poems.txt --validate_file_path=none --epochs=10000 --training_echo_interval=500 --validate_echo_interval=200 --training_save_interval=5000 >logs/train.out 2>&1 & echo $! > .trainpid \
     && echo [train started] \
     && busybox tail -f logs/train.out
 fi
