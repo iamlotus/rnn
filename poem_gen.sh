7555321002731f59af37f77b050807d2e2a1c653
lotus.jzx <lotus.jzx@alibaba-inc.com>
2018-09-10 19:54:59 +0800

    modified

--- poem_gen.sh
+++ poem_gen.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-python3 poem.py --mode=gen --cell_type=lstm --cuda_visible_devices=1
+python3 poem.py --mode=gen --cell_type=lstm --rnn_size=256 --cuda_visible_devices=1
 
 
---
+++ poem_gen2.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+python3 poem.py --mode=gen --cell_type=lstm --cuda_visible_devices=1
+
+
