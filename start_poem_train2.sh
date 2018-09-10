5f2a9110f5faa10ea3044a888dde8d4aa72eef3d
lotus.jzx <lotus.jzx@alibaba-inc.com>
2018-09-10 20:36:09 +0800

    modified

--- start_poem_train2.sh
+++ start_poem_train2.sh
@@ -9,12 +9,12 @@ if [ -f ".trainpid" ]; then
     else
         rm .trainpid \
         && echo [remove dead pid `cat .trainpid`] \
-        && nohup python3 poem.py --mode=train --cell_type=lstm --learning_rate=0.001 --batch_size=2 --train_file_path=./data/validate_poems.txt --epochs=100 --training_echo_interval=10 --validate_echo_interval=200 --training_save_interval=100 --validate_file_path=none >logs/train.out 2>&1 & echo $! > .trainpid \
+        && nohup python3 poem.py --mode=train --cell_type=lstm --learning_rate=0.001 --batch_size=48 --train_file_path=./data/validate_poems.txt --epochs=10000 --training_echo_interval=10 --validate_echo_interval=200 --training_save_interval=100 --validate_file_path=none >logs/train.out 2>&1 & echo $! > .trainpid \
         && echo [train started] \
         && tail -f logs/train.out
     fi
 else
-    nohup python3 poem.py --mode=train --cell_type=lstm --learning_rate=0.001 --batch_size=2 --train_file_path=./data/validate_poems.txt --epochs=100 --training_echo_interval=10 --validate_echo_interval=200 --training_save_interval=100 --validate_file_path=none >logs/train.out 2>&1 & echo $! > .trainpid \
+    nohup python3 poem.py --mode=train --cell_type=lstm --learning_rate=0.001 --batch_size=48 --train_file_path=./data/validate_poems.txt --epochs=10000 --training_echo_interval=10 --validate_echo_interval=200 --training_save_interval=100 --validate_file_path=none >logs/train.out 2>&1 & echo $! > .trainpid \
     && echo [train started] \
     && tail -f logs/train.out
 fi
