2f87b579ef50adccda615a3afe2e8c14c5d0a016
lotus.jzx <lotus.jzx@alibaba-inc.com>
2018-09-10 17:45:14 +0800

    modified

--- tensorboard.sh
+++ tensorboard.sh
@@ -3,7 +3,7 @@ if [ -f ".tensorboardpid" ]; then
     if [ -d /proc/`cat .tensorboardpid` ]; then
         echo found running tensorboard pid `cat .tensorboardpid`
     else
-        echo [remove dead pid `cat .tensorboard`] \
+        echo [remove dead pid `cat .tensorboardpid`] \
         && rm .tensorboardpid \
         && nohup tensorboard --port 10086 --logdir=logs/ >logs/tensorboard.out 2>&1 & echo $! > .tensorboardpid \
         && echo [tensorboard started]
