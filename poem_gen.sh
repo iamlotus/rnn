7399c543ad0084d9de6861ad08cc3aa39c2582cd
lotus.jzx <lotus.jzx@alibaba-inc.com>
2018-09-10 17:14:58 +0800

    modified

--- poem_gen.sh
+++ poem_gen.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-python3 poem.py --mode=gen --cuda_visible_devices=1 --batch_size=1 --train_file_path=./data/small_poems.txt --validate_file_path=none
+python3 poem.py --mode=gen --cuda_visible_devices=1
 
 
