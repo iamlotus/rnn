f726f299bc10dcfc78c97e8cee247522569f75cb
lotus.jzx <lotus.jzx@alibaba-inc.com>
2018-09-13 16:52:06 +0800

    modified

--- start_jinyong_train.sh
+++ start_jinyong_train.sh
@@ -7,12 +7,12 @@ if [ -f ".jytrainpid" ]; then
     else
         rm .jytrainpid \
         && echo [remove dead pid `cat .jytrainpid`] \
-        && nohup python3 jinyong.py --cuda_visible_devices=1 --mode=train --cell_type=lstm --rnn_size=256 --num_layers=3 --learning_rate=0.00001 --batch_size=64 --epochs=20000 --training_echo_interval=10 --gen_line_interval=1000 --save_checkpoints_steps=1000 >logs/jinyong/train.out 2>&1 & echo $! > .jytrainpid \
+        && nohup python3 jinyong.py --cuda_visible_devices=1 --mode=train --cell_type=lstm --rnn_size=256 --num_layers=3 --learning_rate=0.00001 --batch_size=64 --epochs=20000 --training_echo_interval=1 --gen_line_interval=1000 --save_checkpoints_steps=1000 >logs/jinyong/train.out 2>&1 & echo $! > .jytrainpid \
         && echo [train started] \
         && busybox tail -f logs/jinyong/train.out
     fi
 else
-    nohup python3 jinyong.py --cuda_visible_devices=1 --mode=train --cell_type=lstm --rnn_size=256 --num_layers=3 --learning_rate=0.00001 --batch_size=64 --epochs=20000 --training_echo_interval=10 --gen_line_interval=1000 --save_checkpoints_steps=1000 >logs/jinyong/train.out 2>&1 & echo $! > .jytrainpid \
+    nohup python3 jinyong.py --cuda_visible_devices=1 --mode=train --cell_type=lstm --rnn_size=256 --num_layers=3 --learning_rate=0.00001 --batch_size=64 --epochs=20000 --training_echo_interval=1 --gen_line_interval=1000 --save_checkpoints_steps=1000 >logs/jinyong/train.out 2>&1 & echo $! > .jytrainpid \
     && echo [train started] \
     && busybox tail -f logs/jinyong/train.out
 fi
