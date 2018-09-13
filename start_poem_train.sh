cad0c0d29c5de7e10e5c0c7e494032b6a4e7f2d0
lotus.jzx <lotus.jzx@alibaba-inc.com>
2018-09-13 16:44:41 +0800

    modified

--- start_poem_train.sh
+++ start_poem_train.sh
@@ -11,12 +11,12 @@ if [ -f ".poemtrainpid" ]; then
         && echo [remove dead pid `cat .poemtrainpid`] \
         && nohup python3 poem.py --mode=train --cell_type=lstm --rnn_size=256 --learning_rate=0.00001 --batch_size=4096 --train_file_path=./data/poems.txt --validate_file_path=none --epochs=20000 --training_echo_interval=1 --validate_echo_interval=200 --training_save_interval=100 >logs/poem/train.out 2>&1 & echo $! > .poemtrainpid \
         && echo [train started] \
-        && busybox tail -f logs/train.out
+        && busybox tail -f logs/poem/train.out
     fi
 else
     nohup python3 poem.py --mode=train --cell_type=lstm --rnn_size=256 --learning_rate=0.00001 --batch_size=4096 --train_file_path=./data/poems.txt --validate_file_path=none --epochs=20000 --training_echo_interval=1 --validate_echo_interval=200 --training_save_interval=100 >logs/poem/train.out 2>&1 & echo $! > .poemtrainpid \
     && echo [train started] \
-    && busybox tail -f logs/train.out
+    && busybox tail -f logs/poem/train.out
 fi
 
 
