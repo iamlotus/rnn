4ee1bcd008b7a407517e8287c09fd4fe9b79c9ee
lotus.jzx <lotus.jzx@alibaba-inc.com>
2018-09-13 16:41:44 +0800

    modified

--- clean_then_start_jinyong.sh
+++ clean_then_start_jinyong.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-
+mkdir -p logs/jinyong model/jinyong
 rm -rf logs/jinyong/*  model/jinyong/*
 ./stop_jinyong_train.sh
 ./start_jinyong_train.sh
--- clean_then_start_poem.sh
+++ clean_then_start_poem.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-
-rm -rf logs/* model/*
+mkdir -p logs/poem model/poem
+rm -rf logs/poem/* model/poem/*
 ./stop_poem_train.sh
 ./start_poem_train.sh
 
