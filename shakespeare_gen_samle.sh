1e4710a7ef95b03a622eeb4feefd283ebf46d20f
lotus.jzx <lotus.jzx@alibaba-inc.com>
2018-09-26 21:49:32 +0800

    modified

--- shakespeare_gen_samle.sh
+++ shakespeare_gen_samle.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-python3 rnn_tf.py --mode=gen --gpu=4 --input_path=data/shakespeare.txt  --cell_type=gru --rnn_size=128 --num_layers=2
+python3 rnn_tf.py --mode=gen --gpu=4 --encoding=utf --input_path=data/shakespeare.txt  --cell_type=gru --rnn_size=128 --num_layers=2
