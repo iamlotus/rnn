759dde861381b1948cbb21dc32ff6020314b22c8
lotus.jzx <lotus.jzx@alibaba-inc.com>
2018-09-26 16:56:32 +0800

    modified

--- jinyong_retrain.sh
+++ jinyong_retrain.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 mkdir -p logs/jinyong model/jinyong
 rm -rf logs/jinyong/*  model/jinyong/*
+rm -rf data/jinyong.train data/jinyong.dict data/jinyong.validate
 ./jinyong_stop_train.sh
 ./jinyong_start_train.sh
 
